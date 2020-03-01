b257c559e8d1177edb7228a28666f49681d407fc
Luciano Callero <lucianocallero@gmail.com>
2020-03-01 15:49:15 -0300

    Basic crud.

--- src/main/java/com/boilerplate/payment/PaymentController.java
+++ src/main/java/com/boilerplate/payment/PaymentController.java
@@ -3,7 +3,10 @@ package com.boilerplate.payment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,13 +14,30 @@ class PaymentController {
 
     final PaymentService service = new PaymentService();
 
-    @RequestMapping("/payments")
-    public ResponseEntity<Object> listPayments() {
-        return new ResponseEntity<>(service.listPayments(), HttpStatus.OK);
+    @RequestMapping(value = "/payments")
+    public ResponseEntity<Object> list() {
+        return new ResponseEntity<>(service.list(), HttpStatus.OK);
     }
 
-    @RequestMapping("/payments/{id}")
-    public ResponseEntity<Object> paymentPayments(@PathVariable String id) {
-        return new ResponseEntity<>(service.getPayment(id), HttpStatus.OK);
+    @RequestMapping(value = "/payments/{id}")
+    public ResponseEntity<Object> get(@PathVariable String id) {
+        return new ResponseEntity<>(service.get(id), HttpStatus.OK);
     }
+
+    @RequestMapping(value = "/payments/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Object> update(@PathVariable String id, @RequestBody Payment payment) {
+        return new ResponseEntity<>(service.update(id, payment), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/payments", method = RequestMethod.POST)
+    public ResponseEntity<Object> create(@RequestBody Payment payment) {
+        return new ResponseEntity<>(service.create(payment), HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/payments/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable String id) {
+        service.delete(id);
+    }
+
 }
--- src/main/java/com/boilerplate/payment/PaymentService.java
+++ src/main/java/com/boilerplate/payment/PaymentService.java
@@ -3,6 +3,7 @@ package com.boilerplate.payment;
 import java.util.Collection;
 import java.util.HashMap;
 
+import com.boilerplate.exceptions.ConflictException;
 import com.boilerplate.exceptions.NotFoundException;
 
 public class PaymentService {
@@ -15,19 +16,42 @@ public class PaymentService {
         paymentsMap.put(p2.id, p2);
     }
 
-    Payment createPayment(Payment p) {
-        return p;
-    }
-
-    Collection<Payment> listPayments() {
+    Collection<Payment> list() {
         return paymentsMap.values();
     }
 
-    Payment getPayment(String id) {
+    Payment get(String id) {
         if (!paymentsMap.containsKey(id)) {
             throw new NotFoundException("No payment found with ID " + id);
         }
         return paymentsMap.get(id);
+    }
+
+    Payment update(String id, Payment p) {
+        if (!id.equals(p.id)) {
+            throw new ConflictException("Path parameter ID " + id + " does not match body ID " + p.id);
+        }
+        if (!paymentsMap.containsKey(p.id)) {
+            throw new NotFoundException("No payment found with ID " + p.id);
+        }
+        paymentsMap.put(p.id, p);
+        return p;
+    }
+
+    Payment create(Payment p) {
+        if (paymentsMap.containsKey(p.id)) {
+            throw new ConflictException("Payment with ID " + p.id + " already exists");
+        }
+        Payment newPayment = new Payment(p);
+        paymentsMap.put(newPayment.id, newPayment);
+        return newPayment;
+    }
+
+    void delete(String id) {
+        if (!paymentsMap.containsKey(id)) {
+            throw new NotFoundException("No payment found with ID " + id);
+        }
+        paymentsMap.remove(id);
     }
 
     public boolean liquidate(Payment p) {
