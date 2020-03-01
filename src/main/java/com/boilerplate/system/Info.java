e2e15f8e56e308a12aac74f93a7bae6363eb429a
Luciano Callero <lucianocallero@gmail.com>
2020-03-01 15:05:07 -0300

    Using a logger.

--- src/main/java/com/boilerplate/system/Info.java
+++ src/main/java/com/boilerplate/system/Info.java
@@ -1,5 +1,8 @@
 package com.boilerplate.system;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,12 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Info {
 
+    private Logger log = LoggerFactory.getLogger(Info.class);
+
     @Value("${spring.application.name}")
     private String name;
 
     @RequestMapping("/info")
     public String info() {
+        log.trace("A TRACE Message from " + name);
         return name;
     }
-
 }
